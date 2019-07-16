04d113c3ea77ffbfdb98ac4a3804d40e48286295
RonKbS <stephen.bamwite@gmail.com>
2019-07-16 12:25:19 +0300

    use groupby method

--- manipulate_data.py
+++ manipulate_data.py
@@ -12,9 +12,20 @@ cos_t = np.cos(t)
 # print(pandas.DataFrame({'t': t, 'sin': sin_t, 'cos': cos_t}))
 
 # to manipulate data
-print(data.shape)
-print(data.columns)
+# print(data.shape)
+# print(data.columns)
 # print(data['Gender'])
 
 # Simpler selector
-print(data[data['Gender'] == 'Female']['VIQ'].mean())
+# print(data[data['Gender'] == 'Female']['VIQ'].mean())
+
+# return count, mean, std, min, max, 25%, 50% and 75% percentile
+# print(data.describe())
+
+# split dataframe on values of categorical variables
+groupby_gender = data.groupby('Gender')
+for gender, value in groupby_gender['VIQ']:
+    print((gender, value.mean(), value.count()))
+
+for gender, value in groupby_gender['VIQ']:
+    print(gender, value)
